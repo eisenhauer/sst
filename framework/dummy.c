c26b5e22ff8564255c1ca96f387badb50ccb12fe
Greg Eisenhauer <eisen@cc.gatech.edu>
2017-07-28 16:32:44 -0400

    Make the dummy data a bit more consistent.  Still compile-time size of 10240 bytes per rank.  Add Validation call.

--- framework/dummy.c
+++ framework/dummy.c
@@ -7,19 +7,21 @@
 #include "sst.h"
 #include "dummy.h"
 
+#define DATA_SIZE 10240
+
 SstMetadata CreateDummyMetadata(long timestep, int rank, int size)
 {
     SstMetadata meta = malloc(sizeof(struct _SstMetadata));
     struct _SstVarMeta *var;
-    meta->DataSize = 10240;
+    meta->DataSize = DATA_SIZE;
     meta->VarCount = 1;
     var = (struct _SstVarMeta *)malloc(sizeof(struct _SstVarMeta));
     var->VarName = strdup("Array");
     var->DimensionCount = 1;
     var->Dimensions = malloc(sizeof(struct _SstDimenMeta));
-    var->Dimensions->Offset = rank * 10;
-    var->Dimensions->Size = 10;
-    var->Dimensions->GlobalSize = 10 * size;
+    var->Dimensions->Offset = rank * 10240;
+    var->Dimensions->Size = 10240;
+    var->Dimensions->GlobalSize = 10240 * size;
     meta->Vars = var;
     return meta;
 }
@@ -27,11 +29,32 @@ SstMetadata CreateDummyMetadata(long timestep, int rank, int size)
 SstData CreateDummyData(long timestep, int rank, int size)
 {
     SstData data = malloc(sizeof(struct _SstData));
-    data->DataSize = 10 * sizeof(double);
-    data->block = malloc(data->DataSize);
+    data->DataSize = DATA_SIZE;
+    data->block = malloc(DATA_SIZE);
     double *tmp = (double *)data->block;
-    for (int i = 0; i < 10; i++) {
-        tmp[0] = timestep * 1000.0 + rank * 10.0 + i;
+    for (int i = 0; i < DATA_SIZE/8; i++) {
+        tmp[i] = timestep * 1000.0 + rank * 10.0 + i;
     }
     return data;
+}
+
+extern int 
+ValidateDummyData(long timestep, int rank, int size, int offset, void *buffer)
+{
+    int start = (offset +7) / 8;
+    int remainder = offset % 8;
+    double first_double = timestep * 1000.0 + rank * 10.0 + (start - 1);
+    double *tmp  = (double*)((char *)buffer + 8 - remainder);
+    int result = 0;
+
+    if (remainder != 0) {
+        result = memcmp(buffer, ((char*)&first_double)+remainder, 8-remainder);
+    } else {
+        tmp = buffer;
+    }
+    for (int i = start; i < DATA_SIZE/8; i++) {
+        result |= !(*tmp == timestep*1000.0 + rank * 10.0 + i);
+        tmp++;
+    }
+    return result;
 }
--- framework/dummy.h
+++ framework/dummy.h
@@ -1,3 +1,5 @@
 SstMetadata CreateDummyMetadata(long timestep, int rank, int size);
 
 extern SstData CreateDummyData(long timestep, int rank, int size);
+
+extern int ValidateDummyData(long timestep, int rank, int size, int offset, void *buffer);
